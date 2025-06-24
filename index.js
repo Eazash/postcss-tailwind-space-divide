6c58e4e6884d72b9fa43e51b3a0a7bfd9d00df9a
Ezira Ashenafi <eazash22@gmail.com>
2025-06-24 23:45:25 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+/**
+ * @type {import('postcss').PluginCreator}
+ */
+module.exports = (opts = {}) => {
+  // Work with options here
+
+  return {
+    postcssPlugin: 'postcss-tailwind-space-divide-optimizer',
+    /*
+    Root (root, postcss) {
+      // Transform CSS AST here
+    }
+    */
+
+    /*
+    Declaration (decl, postcss) {
+      // The faster way to find Declaration node
+    }
+    */
+
+    /*
+    Declaration: {
+      color: (decl, postcss) {
+        // The fastest way find Declaration node if you know property name
+      }
+    }
+    */
+  }
+}
+
+module.exports.postcss = true
